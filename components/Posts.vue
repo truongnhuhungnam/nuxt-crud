0dd8a137012a56ad4cea6d080a79ddce09dc8791
zoro0307 <truongnhuhungnam@gmail.com>
2021-05-06 19:17:29 +0700

    update

--- components/Posts.vue
+++ components/Posts.vue
@@ -1,52 +1,24 @@
 <template>
     <div class="mt-4">
-        <ul
+        <div
             class="border border-indigo-400 divide-y divide-indigo-400 rounded-sm"
         >
-            <li
+            <SinglePost
                 v-for="post in posts"
                 :key="post.id"
-                class="flex justify-between p-4"
-            >
-                <div class="info">
-                    <p class="text-lg">
-                        <span class="text-indigo-400">Id:</span> {{ post.id }}
-                    </p>
-                    <p class="text-lg">
-                        <span class="text-indigo-400">Title:</span>
-                        {{ post.title }}
-                    </p>
-                    <p class="text-lg">
-                        <span class="text-indigo-400">Author:</span>
-                        {{ post.author }}
-                    </p>
-                </div>
-                <div class="action">
-                    <button
-                        class="rounded-sm bg-green-600 text-white px-2 py-1 w-30 transition hover:bg-green-400"
-                    >
-                        Edit
-                    </button>
-                    <button
-                        class="rounded-sm bg-red-600 text-white px-2 py-1 w-30 transition hover:bg-red-400"
-                    >
-                        Delete
-                    </button>
-                    <nuxt-link
-                        class="rounded-sm bg-indigo-600 text-white px-2 py-1 w-30 inline-block transition hover:bg-indigo-400"
-                        :to="'posts/' + post.id"
-                    >
-                        More Detail
-                    </nuxt-link>
-                </div>
-            </li>
-        </ul>
+                :post-data="post"
+            />
+        </div>
     </div>
 </template>
 
 <script>
 import { mapState } from 'Vuex'
+import SinglePost from './SinglePost.vue'
 export default {
+    components: {
+        SinglePost,
+    },
     computed: {
         ...mapState({
             posts: (state) => state.posts.posts,
---
+++ components/SinglePost.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="flex justify-between p-4">
+        <div class="info">
+            <p class="text-lg">
+                <span class="text-indigo-400">Id:</span> {{ postData.id }}
+            </p>
+            <p class="text-lg">
+                <span class="text-indigo-400">Title:</span>
+                {{ postData.title }}
+            </p>
+            <p class="text-lg">
+                <span class="text-indigo-400">Author:</span>
+                {{ postData.author }}
+            </p>
+        </div>
+        <div class="action">
+            <button
+                class="rounded-sm bg-green-600 text-white px-2 py-1 w-30 transition hover:bg-green-400"
+            >
+                Edit
+            </button>
+            <button
+                class="rounded-sm bg-red-600 text-white px-2 py-1 w-30 transition hover:bg-red-400"
+            >
+                Delete
+            </button>
+            <nuxt-link
+                class="rounded-sm bg-indigo-600 text-white px-2 py-1 w-30 inline-block transition hover:bg-indigo-400"
+                :to="'posts/' + postData.id"
+            >
+                More Detail
+            </nuxt-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        postData: {
+            type: Object,
+            default() {
+                return {
+                    id: '1',
+                    title: 'title',
+                    author: 'author',
+                }
+            },
+        },
+    },
+}
+</script>
