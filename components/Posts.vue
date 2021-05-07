20d5b234bc53869757fda0a109cdf5b2158867f3
zoro0307 <truongnhuhungnam@gmail.com>
2021-05-08 06:47:21 +0700

    update

--- components/Posts.vue
+++ components/Posts.vue
@@ -2,9 +2,11 @@
     <div class="mt-4">
         <button
             class="rounded-sm bg-yellow-500 text-white px-2 py-1 w-30 transition hover:bg-yellow-300"
+            @click="openAddModal"
         >
             Add A Post
         </button>
+        <sweet-modal ref="addModal"> This is a success! </sweet-modal>
         <div
             class="mt-4 border border-indigo-400 divide-y divide-indigo-400 rounded-sm"
         >
@@ -19,9 +21,11 @@
 
 <script>
 import { mapState } from 'Vuex'
+import { SweetModal } from 'sweet-modal-vue'
 import SinglePost from './SinglePost.vue'
 export default {
     components: {
+        SweetModal,
         SinglePost,
     },
     computed: {
@@ -31,6 +35,11 @@ export default {
     },
     mounted() {
         this.$store.dispatch('posts/getPosts')
+    },
+    methods: {
+        openAddModal() {
+            this.$refs.addModal.open()
+        },
     },
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -51,5 +51,11 @@ export default {
     },
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {},
+    build: {
+        babel: {
+            plugins: [
+                ['@babel/plugin-proposal-private-methods', { loose: true }],
+            ],
+        },
+    },
 }
